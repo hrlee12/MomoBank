5b4e4808532a457e9652180cb19399b537272aa7
devjun20 <fwccjs@gmail.com>
2024-03-12 10:41:16 +0900

    chore: bank init

---
+++ backend/bank/src/main/java/com/ssafy/bank/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.ssafy.bank.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import lombok.RequiredArgsConstructor;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(title="뱅킹서비스 API 명세서", version = "v1", description = "뱅킹서비스 API v1 문서입니다.")
+)
+@RequiredArgsConstructor
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public GroupedOpenApi chatOpenApi() {
+        String[] paths = {"/api/v1/**"};
+
+        return GroupedOpenApi.builder()
+                .group("뱅킹서비스 API v1")
+                .pathsToMatch(paths)
+                .build();
+    }
+}
