ae5138617484e11526ef8604abfcf2fc422c5138
명소이 <myeongsoyi@gmail.com>
2024-03-14 13:41:11 +0900

    feat: card dto 생성

---
+++ backend/bank/src/main/java/com/ssafy/bank/card/dto/request/cardRequestDto.java
@@ -0,0 +1,8 @@
+package com.ssafy.bank.card.dto.request;
+
+public record cardRequestDto (
+    int accountId,
+    int productId
+){
+
+}
---
+++ backend/bank/src/main/java/com/ssafy/bank/card/dto/response/productResponseDto.java
@@ -0,0 +1,10 @@
+package com.ssafy.bank.card.dto.response;
+
+public record productResponseDto(
+    String productType,
+    String productName,
+    String productDiscript,
+    String company
+) {
+
+}
