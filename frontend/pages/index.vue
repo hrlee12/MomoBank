b1615f5dee635a25257f04862eee1f1cf946b304
MuRing <11esh@naver.com>
2024-03-14 13:54:09 +0900

    Initial commit

---
+++ frontend/pages/index.vue
@@ -0,0 +1,28 @@
+<script setup>
+// Root path for the entire website
+</script>
+
+<template>
+  <div>
+    <h2>Home</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed tempora error
+      fugiat sit ipsum a earum ad possimus quidem laboriosam?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed tempora error
+      fugiat sit ipsum a earum ad possimus quidem laboriosam?
+    </p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+h2 {
+  margin-top: 20px;
+  font-size: 36px;
+}
+
+p {
+  margin: 20px 0;
+}
+</style>
