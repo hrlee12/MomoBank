0c4f7e52ee53dacdb9ba8184b36e75c8e0a8f1bd
김성수 <zxzxzlxlzlxl@naver.com>
2024-03-18 11:16:22 +0900

    feat: 모임 헤더 작성

--- frontend/layouts/meetingHeader.vue
+++ frontend/layouts/meetingHeader.vue
@@ -1,7 +1,24 @@
+<script setup></script>
+
 <template>
-  <header class="bg-[#50d71e] flex flex-row mt-6 h-11">
-    <div class="basis-1/4 bg-red-100">01</div>
-    <div class="basis-1/4 bg-blue-100">02</div>
-    <div class="basis-1/2 bg-orange-100">03</div>
+  <header class="flex flex-row mt-6 h-11 justify-between">
+    <div class="basis-1/4">
+      <img
+        class="w-10 h-8 ml-4"
+        src="/home/ssafy/code/S10P22A505/frontend/public/icon/arrow-icon.png"
+        alt=""
+      />
+    </div>
+    <div class="items-center text-xl font-semibold">5반 5린이들</div>
+    <div>
+      <div class="flex">
+        <img
+          class="w-7 h-7 mr-4"
+          src="/home/ssafy/code/S10P22A505/frontend/public/icon/bell-icon.png"
+          alt=""
+        />
+        <img class="w-7 h-7 mr-4" src="/icon/home-icon.png" alt="" />
+      </div>
+    </div>
   </header>
 </template>
