def94a13b4ee6b553d15a30e09d5f081e3081c83
김성수 <zxzxzlxlzlxl@naver.com>
2024-03-18 11:19:58 +0900

    fix: icon imageurl getImageUrl 변수로 할당

--- frontend/layouts/meetingHeader.vue
+++ frontend/layouts/meetingHeader.vue
@@ -1,23 +1,19 @@
-<script setup></script>
+<script setup>
+const getImageUrl = (imageName) => {
+  return "/icon/" + imageName;
+};
+</script>
 
 <template>
   <header class="flex flex-row mt-6 h-11 justify-between">
     <div class="basis-1/4">
-      <img
-        class="w-10 h-8 ml-4"
-        src="/home/ssafy/code/S10P22A505/frontend/public/icon/arrow-icon.png"
-        alt=""
-      />
+      <img class="w-10 h-8 ml-4" :src="getImageUrl('arrow-icon.png')" alt="" />
     </div>
     <div class="items-center text-xl font-semibold">5반 5린이들</div>
     <div>
       <div class="flex">
-        <img
-          class="w-7 h-7 mr-4"
-          src="/home/ssafy/code/S10P22A505/frontend/public/icon/bell-icon.png"
-          alt=""
-        />
-        <img class="w-7 h-7 mr-4" src="/icon/home-icon.png" alt="" />
+        <img class="w-7 h-7 mr-4" :src="getImageUrl('bell-icon.png')" alt="" />
+        <img class="w-7 h-7 mr-4" :src="getImageUrl('home-icon.png')" alt="" />
       </div>
     </div>
   </header>
