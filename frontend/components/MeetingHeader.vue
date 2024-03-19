766ed9907680760f471815f75a138b0bedcf21bc
김성수 <zxzxzlxlzlxl@naver.com>
2024-03-19 09:52:54 +0900

    feat: 메인페이지 디자인 작성

---
+++ frontend/components/MeetingHeader.vue
@@ -0,0 +1,34 @@
+<script setup>
+const getImageUrl = (imageName) => {
+  return "/icon/" + imageName;
+};
+</script>
+
+<template>
+  <div class="pt-4">
+    <header class="flex flex-row h-11 justify-between">
+      <div>
+        <img
+          class="w-10 h-8 ml-4"
+          :src="getImageUrl('arrow-icon.png')"
+          alt="arrow-icon 화살표 아이콘"
+        />
+      </div>
+      <div class="text-xl font-semibold ml-8">5반 5린이들</div>
+      <div>
+        <div class="flex">
+          <img
+            class="w-7 h-7 mr-4"
+            :src="getImageUrl('bell-icon.png')"
+            alt="bell-icon"
+          />
+          <img
+            class="w-7 h-7 mr-4"
+            :src="getImageUrl('home-icon.png')"
+            alt="home-icon"
+          />
+        </div>
+      </div>
+    </header>
+  </div>
+</template>
