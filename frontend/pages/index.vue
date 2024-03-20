5b3f3d63ad4562eeff3b127c71878de04c09074a
MuRing <11esh@naver.com>
2024-03-20 10:28:52 +0900

    FIX: temp sungsoo saving code

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,8 +1,11 @@
-<script setup></script>
+<script setup>
+import BankFooter from "@/components/BankFooter.vue";
+</script>
 
 <template>
   <Loading />
   <p>이래도 안돼?</p>
+  <BankFooter />
 </template>
 
 <style lang="scss" scoped></style>
