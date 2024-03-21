4176578252c3b75c58c00b2b624452c8745ece99
김성수 <zxzxzlxlzlxl@naver.com>
2024-03-21 10:43:31 +0900

    feat: 모임 상세피이지 작성

--- frontend/components/layout/GroupsHeader.vue
+++ frontend/components/layout/GroupsHeader.vue
@@ -2,28 +2,32 @@
 const getImageUrl = (imageName) => {
   return "/icon/" + imageName;
 };
+
+const goBack = () => {
+  window.history.back();
+};
 </script>
 
 <template>
   <div class="pt-4">
-    <header class="flex flex-row h-11 justify-between">
-      <div>
+    <header class="flex flex-row justify-between h-11">
+      <div @click="goBack">
         <img
           class="w-10 h-8 ml-4"
           :src="getImageUrl('arrow-icon.png')"
           alt="arrow-icon 화살표 아이콘"
         />
       </div>
-      <div class="text-xl font-semibold ml-8">5반 5린이들</div>
+      <div class="ml-8 text-xl font-semibold">5반 5린이들</div>
       <div>
         <div class="flex">
           <img
-            class="w-7 h-7 mr-4"
+            class="mr-4 w-7 h-7"
             :src="getImageUrl('bell-icon.png')"
             alt="bell-icon"
           />
           <img
-            class="w-7 h-7 mr-4"
+            class="mr-4 w-7 h-7"
             :src="getImageUrl('home-icon.png')"
             alt="home-icon"
           />
