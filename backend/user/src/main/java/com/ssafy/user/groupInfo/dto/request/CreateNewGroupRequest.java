fb4ac8e3df84ef407d420ac66495d6f7520a917e
명소이 <myeongsoyi@gmail.com>
2024-03-21 16:32:31 +0900

    feat: groupInfo controller 수정

---
+++ backend/user/src/main/java/com/ssafy/user/groupInfo/dto/request/CreateNewGroupRequest.java
@@ -0,0 +1,11 @@
+package com.ssafy.user.groupInfo.dto.request;
+
+public record CreateNewGroupRequest(
+    int memberId,
+    String description,
+    String groupName,
+
+    int accountId
+) {
+
+}
