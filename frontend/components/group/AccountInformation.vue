1432555d0ce83248ce32e3dd1803afd37d845c24
김성수 <zxzxzlxlzlxl@naver.com>
2024-03-21 10:37:29 +0900

    feat: 모임 상세페이지 작성(미완)

---
+++ frontend/components/group/AccountInformation.vue
@@ -0,0 +1,17 @@
+<template>
+  <!-- 계좌 번호, 담긴 금액, 숨김 버튼 -->
+  <div class="flex justify-center mt-4">
+    <div class="text-gray-color border-b-2 border-b-gray-color-500 text-[15px]">
+      1234-12-567456
+    </div>
+  </div>
+  <div class="flex justify-center">
+    <div class="font-bold text-[28px]">2,345,678</div>
+    <div class="text-[15px] pt-4 pl-1 pr-2">원</div>
+    <div
+      class="flex items-center justify-center w-8 mt-3 bg-light-gray-color rounded-xl h-7"
+    >
+      <div class="text-gray-color text-[13px]">숨김</div>
+    </div>
+  </div>
+</template>
