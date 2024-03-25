6915c7fa20c2fe21ec91f8de6926d79e1ef951d9
김성수 <zxzxzlxlzlxl@naver.com>
2024-03-25 17:06:44 +0900

    feat: 모임 공지사항 상세보기 페이지 작성

--- frontend/pages/groups/announcement.vue
+++ frontend/pages/groups/announcement.vue
@@ -20,14 +20,36 @@ const getImageUrl = (imageName, idx) => {
       </div>
     </div>
 
+    <nuxt-link to="/groups/announcement-detail">
+      <div class="flex justify-center">
+        <div
+          class="flex flex-col justify-center-center w-11/12 h-11 mt-8 pl-5 border-b-[1px] border-light-gray-color"
+        >
+          <div class="flex items-center pb-2 text-sm font-bold h-1/6">
+            <div class="w-9/12 overflow-hidden whitespace-nowrap text-ellipsis">
+              [투표] 드디어 일본 여행 Let's go!!!
+            </div>
+          </div>
+          <div class="mt-4 text-xs">
+            <div class="flex items-center">
+              <div>2024.3.12</div>
+              <div class="ml-2">김성수</div>
+              <div class="w-3 h-3 ml-1">
+                <img :src="getImageUrl('blue-crown.png', 0)" alt="파란 왕관" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nuxt-link>
+
     <div class="flex justify-center">
       <div
         class="flex flex-col justify-center-center w-11/12 h-11 mt-8 pl-5 border-b-[1px] border-light-gray-color"
       >
         <div class="flex items-center pb-2 text-sm font-bold h-1/6">
-          <div class="pr-1">[투표]&nbsp;</div>
           <div class="w-9/12 overflow-hidden whitespace-nowrap text-ellipsis">
-            드디어 일본 여행 Let's go!!!
+            [투표] 다음 모임 장소 투표
           </div>
         </div>
         <div class="mt-4 text-xs">
@@ -46,30 +68,9 @@ const getImageUrl = (imageName, idx) => {
         class="flex flex-col justify-center-center w-11/12 h-11 mt-8 pl-5 border-b-[1px] border-light-gray-color"
       >
         <div class="flex items-center pb-2 text-sm font-bold h-1/6">
-          <div class="pr-1">[투표]&nbsp;</div>
-          <div class="w-9/12 overflow-hidden whitespace-nowrap text-ellipsis">
-            다음 모임 장소 투표
-          </div>
-        </div>
-        <div class="mt-4 text-xs">
-          <div class="flex items-center">
-            <div>2024.3.12</div>
-            <div class="ml-2">김성수</div>
-            <div class="w-3 h-3 ml-1">
-              <img :src="getImageUrl('blue-crown.png', 0)" alt="파란 왕관" />
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="flex justify-center">
-      <div
-        class="flex flex-col justify-center-center w-11/12 h-11 mt-8 pl-5 border-b-[1px] border-light-gray-color"
-      >
-        <div class="flex items-center pb-2 text-sm font-bold h-1/6">
-          <div class="pr-1">[중요]&nbsp;</div>
-          <div class="w-9/12 overflow-hidden whitespace-nowrap text-ellipsis">
-            피드에 욕설하시면안됩니다. 자제 부탁드립니다.!!!!!!!!!!!!!!!!!!
+          <div class="w-11/12 overflow-hidden whitespace-nowrap text-ellipsis">
+            [중요] 피드에 욕설하시면안됩니다. 자제
+            부탁드립니다.!!!!!!!!!!!!!!!!!!
           </div>
         </div>
         <div class="mt-4 text-xs">
@@ -90,7 +91,7 @@ const getImageUrl = (imageName, idx) => {
         <div class="flex items-center pb-2 text-sm font-bold h-1/6">
           <div class="pr-1">[수요조사]&nbsp;</div>
           <div class="w-9/12 overflow-hidden whitespace-nowrap text-ellipsis">
-            다음 모임 참석자 수요조사
+            [수요조사] 다음 모임 참석자 수요조사
           </div>
         </div>
         <div class="mt-4 text-xs">
