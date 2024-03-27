0e4e831cef01b43d89d63102203214bef84b60f4
minwoo-kwak <minwoo.kwak540@gmail.com>
2024-03-27 17:46:39 +0900

    feat: 피드 v1

---
+++ backend/community/src/main/java/com/ssafy/community/feed/domain/Feed.java
@@ -0,0 +1,50 @@
+package com.ssafy.community.feed.domain;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Getter
+@Setter
+@Entity
+@Table(name = "feed")
+@Schema(description = "피드 정보")
+public class Feed {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(description = "피드 ID", example = "1")
+    private Integer feedId;
+
+    @Schema(description = "내용", example = "피드 내용입니다.")
+    private String content;
+
+    @Schema(description = "좋아요 수", example = "100")
+    private Integer likesCount;
+
+    @Schema(description = "댓글 수", example = "50")
+    private Integer commentsCount;
+
+    @Column(nullable = false, updatable = false)
+    @Schema(description = "생성일", example = "2023-03-15T12:00:00")
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    @Schema(description = "업데이트 일자", example = "2023-03-16T12:00:00")
+    private LocalDateTime updatedAt;
+
+    @Schema(description = "삭제 여부", example = "false")
+    private Boolean isDeleted;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "group_member_id")
+    private GroupMember groupMember;
+
+
+}
---
+++ backend/community/src/main/java/com/ssafy/community/feed/domain/repository/FeedRepository.java
@@ -0,0 +1,29 @@
+package com.ssafy.community.feed.domain.repository;
+
+import com.ssafy.community.feed.domain.Comments;
+import com.ssafy.community.feed.domain.Feed;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface FeedRepository extends JpaRepository<Feed, Integer> {
+//    @Query("SELECT f FROM Feed f WHERE f.isDeleted = false")
+//    Page<Feed> findAll(Pageable pageable);
+
+    @Query("SELECT f FROM Feed f WHERE f.groupMember.groupInfo.groupId = :groupId AND f.isDeleted = false")
+    Page<Feed> findByGroupId(@Param("groupId") Integer groupId, Pageable pageable);
+
+    Optional<Feed> findByFeedId(Integer feedId);
+
+    void deleteById(Integer feedId);
+
+    @Query("SELECT c FROM Comments c WHERE c.commentId = :commentId")
+    Optional<Comments> findCommentById(@Param("commentId") Integer commentId);
+
+
+}
