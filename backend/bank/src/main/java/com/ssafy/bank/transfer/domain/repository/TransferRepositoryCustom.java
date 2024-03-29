d8e0d56c0cc05ee70bbb7528a43f488fa273d6d9
명소이 <myeongsoyi@gmail.com>
2024-03-29 17:52:53 +0900

    feat: transfer repository 생성

---
+++ backend/bank/src/main/java/com/ssafy/bank/transfer/domain/repository/TransferRepositoryCustom.java
@@ -0,0 +1,5 @@
+package com.ssafy.bank.transfer.domain.repository;
+
+public interface TransferRepositoryCustom {
+
+}
---
+++ backend/bank/src/main/java/com/ssafy/bank/transfer/domain/repository/TransferRepositoryImpl.java
@@ -0,0 +1,8 @@
+package com.ssafy.bank.transfer.domain.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class TransferRepositoryImpl implements TransferRepositoryCustom{
+
+}
