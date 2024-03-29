b791a1537e0e1a828eb4b5564b6eae92db46fd29
이효리 <gyfl5542@naver.com>
2024-03-29 13:44:55 +0900

    fix: kafkaTestController의 Kafktemplate<String, Object>로 변경

--- backend/user/src/main/java/com/ssafy/user/kafkaTest/controlller/KafkaTestController.java
+++ backend/user/src/main/java/com/ssafy/user/kafkaTest/controlller/KafkaTestController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class KafkaTestController {
 
     @Autowired
-    private KafkaTemplate<String, String> kafkaTemplate;
+    private KafkaTemplate<String, Object> kafkaTemplate;
 
     @GetMapping("/send")
     public String sendMessage() {
