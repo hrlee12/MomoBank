7bc5472cf4774bf8ba274d45ec719af6152261a1
minwoo-kwak <minwoo.kwak540@gmail.com>
2024-03-30 16:36:38 +0900

    fix: allowedOrigins cannot contain *

--- backend/community/src/main/java/com/ssafy/community/config/WebMvcConfig.java
+++ backend/community/src/main/java/com/ssafy/community/config/WebMvcConfig.java
@@ -10,7 +10,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*") // 모든 출처 허용
+                .allowedOrigins("http://localhost:8083", "http://j10a505.p.ssafy.io") // 모든 출처 허용
                 .allowedMethods("*") // 모든 HTTP 메소드 허용
                 .allowedHeaders("*") // 모든 헤더 허용
                 .allowCredentials(true) // 쿠키를 포함시키기 위해 필요
