c658c362a7338f6f5d55e8d214d647cf60020b82
minwoo-kwak <minwoo.kwak540@gmail.com>
2024-03-30 16:28:42 +0900

    fix: swagger cors error

--- backend/community/src/main/java/com/ssafy/community/config/WebMvcConfig.java
+++ backend/community/src/main/java/com/ssafy/community/config/WebMvcConfig.java
@@ -10,7 +10,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE");
+                .allowedOrigins("*") // 모든 출처 허용
+                .allowedMethods("*") // 모든 HTTP 메소드 허용
+                .allowedHeaders("*") // 모든 헤더 허용
+                .allowCredentials(true) // 쿠키를 포함시키기 위해 필요
+                .maxAge(3600); // 사전 요청(pre-flight request) 캐싱 시간
     }
 }
