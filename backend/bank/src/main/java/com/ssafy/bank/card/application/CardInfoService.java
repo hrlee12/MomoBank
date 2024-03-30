8d9bf5db595dea7c057aa2d10e9605806cbb5f26
명소이 <myeongsoyi@gmail.com>
2024-03-30 21:19:50 +0900

    fix: http method 수정 및 card service, controller return 변경

--- backend/bank/src/main/java/com/ssafy/bank/card/application/CardInfoService.java
+++ backend/bank/src/main/java/com/ssafy/bank/card/application/CardInfoService.java
@@ -2,8 +2,6 @@ package com.ssafy.bank.card.application;
 
 import com.ssafy.bank.account.domain.Account;
 import com.ssafy.bank.account.domain.repository.AccountRepository;
-import com.ssafy.bank.account.dto.response.AccountResponse;
-import com.ssafy.bank.card.domain.Card;
 import com.ssafy.bank.card.domain.CardInfo;
 import com.ssafy.bank.card.domain.CardProduct;
 import com.ssafy.bank.card.domain.repository.CardInfoRepository;
@@ -21,56 +19,55 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 @Transactional
 public class CardInfoService {
-  private final MemberRepository memberRepository;
-  private final AccountRepository accountRepository;
-  private final CardProductRepository cardProductRepository;
-  private final CardInfoRepository cardInfoRepository;
 
-  public CardInfoResponse createCardInfo(int memberId, int cardProductId, int accountId){
-    Member member = memberRepository.findById(memberId)
-        .orElseThrow(() -> new CustomException(ErrorCode.NO_SUCH_MEMBER));
-    if (member.isDeleted()) {
-      throw new CustomException(ErrorCode.DELETED_MEMBER);
+    private final MemberRepository memberRepository;
+    private final AccountRepository accountRepository;
+    private final CardProductRepository cardProductRepository;
+    private final CardInfoRepository cardInfoRepository;
+
+    public CardInfoResponse createCardInfo(int memberId, int cardProductId, int accountId) {
+        Member member = memberRepository.findById(memberId)
+            .orElseThrow(() -> new CustomException(ErrorCode.NO_SUCH_MEMBER));
+        if (member.isDeleted()) {
+            throw new CustomException(ErrorCode.DELETED_MEMBER);
+        }
+
+        Account account = accountRepository.findById(accountId)
+            .orElseThrow(() -> new CustomException(ErrorCode.NO_SUCH_ACCOUNT));
+        if (account.isDeleted()) {
+            throw new CustomException(ErrorCode.DELETED_ACCOUNT);
+        }
+
+        CardProduct cardProduct = cardProductRepository.findById(cardProductId)
+            .orElseThrow(() -> new CustomException(ErrorCode.NO_SUCH_CARD_PRODUCT));
+        if (cardProduct.isDeleted()) {
+            throw new CustomException(ErrorCode.DELETED_CARD_PRODUCT);
+        }
+
+        CardInfo cardInfo = CardInfo.builder()
+            .cardInfoNum("505-01")
+            .cardProduct(cardProduct)
+            .account(account)
+            .build();
+        cardInfoRepository.save(cardInfo);
+
+        return CardInfoResponse.from(cardInfo,
+            cardInfo.getCardProduct().getBank() == null ?
+                cardInfo.getCardProduct().getCard().getCardName() :
+                cardInfo.getCardProduct().getBank().getBankName());
     }
 
-    Account account = accountRepository.findById(accountId)
-        .orElseThrow(() -> new CustomException(ErrorCode.NO_SUCH_ACCOUNT));
-    if (account.isDeleted()) {
-      throw new CustomException(ErrorCode.DELETED_ACCOUNT);
-    }
+    public CardInfoResponse deleteCardInfo(int cardInfoId) {
+        CardInfo cardInfo = cardInfoRepository.findById(cardInfoId)
+            .orElseThrow(() -> new CustomException(ErrorCode.NO_SUCH_CARD_INFO));
+        if (cardInfo.isDeleted()) {
+            throw new CustomException(ErrorCode.DELETED_CARD_INFO);
+        }
+        cardInfo.softDelete();
 
-    CardProduct cardProduct = cardProductRepository.findById(cardProductId)
-        .orElseThrow(() -> new CustomException(ErrorCode.NO_SUCH_CARD_PRODUCT));
-    if (cardProduct.isDeleted()) {
-      throw new CustomException(ErrorCode.DELETED_CARD_PRODUCT);
+        return CardInfoResponse.from(cardInfo,
+            cardInfo.getCardProduct().getBank() == null ?
+                cardInfo.getCardProduct().getCard().getCardName() :
+                cardInfo.getCardProduct().getBank().getBankName());
     }
-
-    CardInfo cardInfo = CardInfo.builder()
-        .cardInfoNum("505-01")
-        .cardProduct(cardProduct)
-        .account(account)
-        .build();
-    cardInfoRepository.save(cardInfo);
-
-    if(cardInfo.getCardProduct().getBank() != null){
-      return CardInfoResponse.from(cardInfo, cardInfo.getCardProduct().getBank().getBankName());
-    }else{
-      return CardInfoResponse.from(cardInfo, cardInfo.getCardProduct().getCard().getCardName());
-    }
-  }
-
-  public CardInfoResponse deleteCardInfo(int cardInfoId){
-    CardInfo cardInfo = cardInfoRepository.findById(cardInfoId)
-        .orElseThrow(() -> new CustomException(ErrorCode.NO_SUCH_CARD_INFO));
-    if(cardInfo.isDeleted()) {
-      throw new CustomException(ErrorCode.DELETED_CARD_INFO);
-    }
-    cardInfo.softDelete();
-
-    if(cardInfo.getCardProduct().getBank() != null){
-      return CardInfoResponse.from(cardInfo, cardInfo.getCardProduct().getBank().getBankName());
-    }else{
-      return CardInfoResponse.from(cardInfo, cardInfo.getCardProduct().getCard().getCardName());
-    }
-  }
 }
