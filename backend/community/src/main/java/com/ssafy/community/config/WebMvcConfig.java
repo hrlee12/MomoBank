045db22521a0eeaa5a15b9bac9056ea43dd12060
곽민우 <rhkr8678@naver.com>
2024-04-01 09:33:48 +0900

    fix: community cors

--- backend/community/src/main/java/com/ssafy/community/config/WebMvcConfig.java
+++ backend/community/src/main/java/com/ssafy/community/config/WebMvcConfig.java
@@ -10,9 +10,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8083", "http://j10a505.p.ssafy.io") // 모든 출처 허용
-                .allowedMethods("*") // 모든 HTTP 메소드 허용
-                .allowedHeaders("*") // 모든 헤더 허용
+                .allowedOrigins("http://localhost:8083","http://localhost:3000", "http://j10a505.p.ssafy.io", "https://j10a505.p.ssafy.io") // 모든 출처 허용
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS", "PATCH", "TRACE") // 모든 HTTP 메소드 허용
+                .allowedHeaders("Content-Type", "Authorization") // 모든 헤더 허용
                 .allowCredentials(true) // 쿠키를 포함시키기 위해 필요
                 .maxAge(3600); // 사전 요청(pre-flight request) 캐싱 시간
     }
