6babac23db256cccceed8fe0ba0aa2266e292370
명소이 <myeongsoyi@gmail.com>
2024-04-01 10:55:09 +0900

    fix: 계좌 조회

--- backend/user/src/main/java/com/ssafy/user/bank/presentation/BankController.java
+++ backend/user/src/main/java/com/ssafy/user/bank/presentation/BankController.java
@@ -18,6 +18,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -45,10 +46,10 @@ public class BankController {
     }
 
     @Operation(summary = "본인 계좌 조회", description = "사용자의 계좌를 조회합니다.")
-    @GetMapping("/my-accounts")
-    public ResponseEntity<?> getMyAccount(@RequestBody GetMyAccountRequest request) {
+    @GetMapping("/my-accounts/{id}")
+    public ResponseEntity<?> getMyAccount(@PathVariable int id) {
         return CommonResponse.toResponseEntity(HttpStatus.OK, "사용자 계좌 조회 성공",
-            bankService.getMyAccount(request.memberId()));
+            bankService.getMyAccount(id));
     }
 
     @Operation(summary = "계좌 상세 조회", description = "특정 계좌 상세를 조회합니다.")
