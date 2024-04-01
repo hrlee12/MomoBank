3bcfeb4e20be134ddbea8baf429f9fda8e62f047
minwoo-kwak <minwoo.kwak540@gmail.com>
2024-04-01 09:59:58 +0900

    fix: user swagger 설정

--- backend/user/src/main/java/com/ssafy/user/config/SwaggerConfig.java
+++ backend/user/src/main/java/com/ssafy/user/config/SwaggerConfig.java
@@ -2,26 +2,24 @@ package com.ssafy.user.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
-import lombok.RequiredArgsConstructor;
-import org.springdoc.core.models.GroupedOpenApi;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import static java.awt.SystemColor.info;
+import java.util.List;
 
 @OpenAPIDefinition(
         info = @Info(title="유저서비스 API 명세서", version = "v1", description = "유저서비스 API v1 문서입니다.")
 )
-@RequiredArgsConstructor
 @Configuration
 public class SwaggerConfig {
     @Bean
-    public GroupedOpenApi chatOpenApi() {
-        String[] paths = {"/**"};
-
-        return GroupedOpenApi.builder()
-                .group("유저서비스 API v1")
-                .pathsToMatch(paths)
-                .build();
+    public OpenAPI customOpenAPI(@Value("${openapi.service.url}") String url) {
+        return new OpenAPI()
+                .servers(List.of(new Server().url(url)));
     }
+
+    // 여기에 필요한 경우 GroupedOpenApi 빈 설정을 유지하거나 추가할 수 있습니다.
 }
